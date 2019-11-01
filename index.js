27d2530d0300861f92c5d216c51d10f1283960f6
sage-jordan <sagemjrodan@gmail.com>
2019-10-31 22:21:23 -0400

    get users works

--- index.js
+++ index.js
@@ -12,62 +12,70 @@ server.listen(5000, () => {
     console.log('=== server listening on port 5000===');
 });
 
-//CREATE A USER
-server.post('/api/users', (req, res) => {
-    const user = res.params.body;
-    db.insert(user)
-        .then(newUser => {
-            res.status(200).json({success: true, newUser})
-        })
-        .catch(err => {
-            res.status(500).json({success:false, message: err})
-        });
-})
+server.use(express.json());
+
+// //CREATE A USER
+// server.post('/api/users', (req, res) => {
+//     const user = res.body;
+//     console.log(user);
+//     db.insert(user)
+//         .then(newUser => {
+//             res.status(200).json({success: true, newUser})
+//         })
+//         .catch(err => {
+//             res.status(500).json({success:false, message: err})
+//         });
+// })
 
 //GET USERS
 server.get('/api/users', (req, res) => { 
-    const users = db.find();
-    res.status(201).send(users);
-});
-
-//GET USER BY ID
-server.get('/api/users/:id', (req, res) => {
-    const id = req.params;
-    db.findById(id)
-        .then(byId => {
-            res.status(202).json({success: true, byId})
-        })
-        .catch(err => {
-            res.status(501).json({success: false, message: `cannot find user with id= ${id}`})
-        })
-});
-
-//UPDATE USER
-server.put('/api/users/:id', (req, res) => {
-    const id = req.params.id;
-    const userInfo = req.body;
-    db.update(id , userInfo)
+    db.find()
         .then(user => {
-            if(user) {
-                res.status(203).json
-            } else {
-                res.status(404).json({success: false, message: `cannot find user with id= ${id}`})
-            }
-        })
-})
-
-//DELETE USER
-server.delete('/api/users/:id', (req, res) => {
-    const {id} = req.params;
-    db.remove(id)
-        .then(deletedUser => {
-            if(deletedUser) {
-                res.status(204).end();
-            } else {
-                res.status(404).json({message: `i could not find id= ${id}`});
-            }
+            res.status(201).json(user)
         })
         .catch(err => {
-            res.status(500).json({success: false, err});
+            res.status(501).json({success: false, message: err})
         });
-})
+});
+
+// //GET USER BY ID
+// server.get('/api/users/:id', (req, res) => {
+//     const id = req.params;
+//     db.findById(id)
+//         .then(byId => {
+//             res.status(202).json({success: true, byId})
+//         })
+//         .catch(err => {
+//             res.status(501).json({success: false, message: `cannot find user with id= ${id}`})
+//         })
+// });
+
+// //UPDATE USER
+// server.put('/api/users/:id', (req, res) => {
+//     const id = req.params.id;
+//     const userInfo = req.body;
+//     db.update(id , userInfo)
+//         .then(user => {
+//             if(user) {
+//                 res.status(203).json
+//             } else {
+//                 res.status(404).json({success: false, message: `cannot find user with id= ${id}`})
+//             }
+//         })
+// })
+
+// //DELETE USER
+// server.delete('/api/users/:id', (req, res) => {
+//     const {id} = req.params;
+//     db.remove(id)
+//         .then(deletedUser => {
+//             if(deletedUser) {
+//                 res.status(204).end();
+//             } else {
+//                 res.status(404).json({message: `i could not find id= ${id}`});
+//             }
+//         })
+//         .catch(err => {
+//             res.status(500).json({success: false, err});
+//         });
+// })
